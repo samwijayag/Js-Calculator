ddc9cbd4977c477bae50d140d3c0592700092f59
samwijayag <sam.wijayag@gmail.com>
2019-03-13 11:44:38 -0700

    added onclick

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@ let operator;
 let flag = 0;
 let display = document.getElementById('display');
 let equalTo = 0;
+
+
+const setValue = (number) => {
+	if(equalTo === 1){
+		clearButton();
+	}
+	if(flag === 0){
+		num1 += number;
+		display.innerHTML += number;
+	}else{
+		num2 += number;
+		display.innerHTML += number;
+	}
+	if(num1.length> 10 || num2.length > 10){
+		display.innerHTML= 'Max limit of digits reached';
+	}
+}
